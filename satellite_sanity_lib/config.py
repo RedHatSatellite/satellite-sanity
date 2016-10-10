4875914ca6c2c06773f624982667e900583eb31a
Jan Hutar <jhutar@redhat.com>
2016-10-10 12:27:29 +0200

    Set logging so by default INFO+ goes to stdout and DEBUG+ to log file

--- satellite_sanity_lib/config.py
+++ satellite_sanity_lib/config.py
@@ -7,9 +7,22 @@ L_DEBUG = logging.DEBUG
 L_INFO = logging.INFO
 L_ERROR = logging.ERROR
 
-logging.basicConfig(level=L_ERROR)
+# From https://docs.python.org/2/howto/logging-cookbook.html#multiple-handlers-and-formatters
 logger = logging.getLogger(__name__)
-
+logger.setLevel(level=logging.DEBUG)
+# Create console handler wich logs only info messages and above by default
+ch = logging.StreamHandler()
+ch.setLevel(logging.INFO)
+formatter = logging.Formatter('%(levelname)-8s %(message)s')
+ch.setFormatter(formatter)
+# Create file handler which logs even debug messages
+fh = logging.FileHandler('/tmp/satellite-sanity.log')
+fh.setLevel(logging.DEBUG)
+formatter = logging.Formatter('%(asctime)s %(name)-12s %(levelname)-8s %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
+fh.setFormatter(formatter)
+# Add the handlers to logger
+logger.addHandler(ch)
+logger.addHandler(fh)
 
 C_PASS = '\033[92m'
 C_WARNING = '\033[93m'
