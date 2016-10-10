68690106ce02db1d42a29116cf70f78f8e4802cc
Jan Hutar <jhutar@redhat.com>
2016-10-10 08:07:52 +0200

    Silence tests.sh output

--- tests.sh
+++ tests.sh
@@ -18,26 +18,26 @@ function bye() {
 ./satellite-sanity --tags general --list-rules | grep --quiet '^Selected tag(s): general$'
 ./satellite-sanity --tags Satellite_5 --list-rules | grep --quiet '^Selected tag(s): Satellite_5$'
 ./satellite-sanity --tags general,Satellite_5 --list-rules | grep --quiet '^Selected tag(s): general, Satellite_5$'
-./satellite-sanity --tags general --list-rules | wc -l | grep '^4$'
+./satellite-sanity --tags general --list-rules | wc -l | grep --quiet '^4$'
 
 set +e
 ./satellite-sanity --tags test >/dev/null && bye "FAIL: Plain run failed" 1
-./satellite-sanity --tags Satellite_5 >/dev/null && bye "FAIL: Plain run failed" 1   # check for this tag should fail
+./satellite-sanity --tags Satellite_5 &>/dev/null && bye "FAIL: Plain run failed" 1   # check for this tag should fail
 ./satellite-sanity --tags test --rules example_fails >/dev/null && bye "FAIL: Run with specified rule failed" 1
 ./satellite-sanity --tags test --rules example_fails 2>&1 \
-  | grep 'Selected rule(s): example_fails'
+  | grep --quiet 'Selected rule(s): example_fails'
 [ $? -eq 1 ] || bye "FAIL: Run with specified rule not prints whats expected" 1
 ./satellite-sanity --nagios-plugin --tags test >/dev/null
 [ $? -eq 2 ] || bye "FAIL: Nagios exit code is not 2"
 set +o pipefail
 ./satellite-sanity --nagios-plugin --tags test 2>&1 \
-  | grep 'CRITICAL- Satellite sanity results: passed: 0, skipped: 0, failed: 1, unknown: 0 | Satellite sanity results: passed: 0, skipped: 0, failed: 1, unknown: 0'
+  | grep --quiet 'CRITICAL- Satellite sanity results: passed: 0, skipped: 0, failed: 1, unknown: 0 | Satellite sanity results: passed: 0, skipped: 0, failed: 1, unknown: 0'
 [ $? -ne 0 ] && bye "FAIL: Nagios output is not correct" 1
 ./satellite-sanity --tags Satellite_5 2>&1 \
-  | grep "ERROR: Some prerequisities were not met. Exiting. Maybe you want to add '--force'?"
+  | grep --quiet "ERROR: Some prerequisities were not met. Exiting. Maybe you want to add '--force'?"
 [ $? -ne 0 ] && bye "FAIL: Failing check should mention '--force'" 1
 ./satellite-sanity --tags Satellite_5 --force 2>&1 \
-  | grep "^.*FAIL.*Taskomatic service is running (sat5_taskomatic_running)$"
+  | grep --quiet "^.*FAIL.*Taskomatic service is running (sat5_taskomatic_running)$"
 [ $? -ne 0 ] && bye "FAIL: Even with '--force' taskomatic check was not ran" 1
 set -o pipefail
 set -e
