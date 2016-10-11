ed18a9abdf4c874361367c35b7594aa4120cc15e
Jan Hutar <jhutar@redhat.com>
2016-10-11 10:37:17 +0200

    Cleanup logging config a bit

--- satellite_sanity_lib/config.py
+++ satellite_sanity_lib/config.py
@@ -2,13 +2,16 @@
 # -*- coding: UTF-8 -*-
 
 import logging
+import logging.handlers
+
+APP = 'satellite-sanity'
 
 L_DEBUG = logging.DEBUG
 L_INFO = logging.INFO
 L_ERROR = logging.ERROR
 
 # From https://docs.python.org/2/howto/logging-cookbook.html#multiple-handlers-and-formatters
-logger = logging.getLogger(__name__)
+logger = logging.getLogger(APP)
 logger.setLevel(level=logging.DEBUG)
 # Create console handler wich logs only info messages and above by default
 ch = logging.StreamHandler()
@@ -16,9 +19,9 @@ ch.setLevel(logging.INFO)
 formatter = logging.Formatter('%(levelname)-8s %(message)s')
 ch.setFormatter(formatter)
 # Create file handler which logs even debug messages
-fh = logging.FileHandler('/tmp/satellite-sanity.log')
+fh = logging.handlers.RotatingFileHandler('/tmp/%s.log' % APP, maxBytes=1024*1024, backupCount=1)
 fh.setLevel(logging.DEBUG)
-formatter = logging.Formatter('%(asctime)s %(name)-12s %(levelname)-8s %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
+formatter = logging.Formatter('%(asctime)s %(name)s %(levelname)s %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
 fh.setFormatter(formatter)
 # Add the handlers to logger
 logger.addHandler(ch)
